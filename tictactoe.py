7a56ad8be1b89a633befe5d9de9d3f6ffc920264
BobHahtaway2 <hathawayrl56@yahoo.com>
2022-09-14 15:51:02 -0600

    tictactoe initial checkin

---
+++ tictactoe.py
@@ -0,0 +1,102 @@
+"""
+    Author: Bob Hathaway
+    Course: CSE 210
+    Class: Fall 2022, Section 18
+    Assignment: W01 Prove: Developer, Tic-Tac_Toe
+"""
+import random
+board_array = []
+board_dimension = 3
+
+def initialization():
+    global board_array
+    global board_dimension
+    board_array = [[0 for i in range(board_dimension)] for j in range(board_dimension)]
+    square_number = 1
+    for i in range(board_dimension):
+        for j in range(board_dimension):
+            board_array[i][j] = square_number
+            square_number += 1
+    if random.random() >= 0.5:
+        current_player = 'X'
+    else:
+        current_player = 'O'
+    return current_player
+        
+def determine_row_column_from_selected_value(selected_value):
+    global board_dimension
+    row_value = (selected_value - 1) // board_dimension
+    column_value = (selected_value - 1) % board_dimension
+    return row_value, column_value
+
+def is_selection_available(selected_square):
+    global board_array
+    global board_dimension
+    row_value, column_value = determine_row_column_from_selected_value(selected_square)
+    if board_array[row_value][column_value] == selected_square: # if value hasn't changed to an x or o
+        return True
+    else:
+        return False
+
+def get_players_selection(player):
+    valid_selection = False
+    while not valid_selection:
+        try:
+            players_selection = int(input (f"{player}'s turn to choose a square (1-9): "))
+            if players_selection >=1 and players_selection <= 9:
+                valid_selection = is_selection_available(players_selection)
+                if not valid_selection:
+                    print("That's not an available square. Try again...")
+            else:
+                print("Oops!  That was not a valid square.  Try again...")
+        except:
+            print("That's not a number.  Try again...")
+    return players_selection
+
+def display_board():
+    print ()
+    global board_array
+    global board_dimension
+    for i in range(board_dimension):
+        for j in range(board_dimension):
+
+            if j < board_dimension - 1:
+                print (f'{board_array[i][j]}', end ='|')
+            elif i < board_dimension - 1:
+                print (f'{board_array[i][j]}')
+                for k in range(0,board_dimension,2):
+                    print('-+', end = '')
+                print('-')
+            else:
+                print(board_array[i][j])
+    print ()
+
+def store_players_selection(current_player, players_selection):
+    global board_array
+    row_value, column_value = determine_row_column_from_selected_value(players_selection)
+    board_array [row_value] [column_value] = current_player
+
+def determine_result_of_play(current_player, players_selection):
+    global board_array
+    row_value, column_value = determine_row_column_from_selected_value(players_selection)
+    if board_array[row_value][0] and board_array[row_value][1]
+
+def main():
+    global board_array
+    game_over = False
+    current_player = initialization()
+    while not game_over:
+        display_board()
+        players_selection = get_players_selection(current_player)
+        print(players_selection)
+        store_players_selection(current_player, players_selection)
+        game_over = determine_result_of_play(current_player, players_selection)
+        if current_player == "X":
+            current_player = "O"
+        else:
+            current_player = "X"
+    print (board_array)
+
+
+if __name__ == "__main__":
+    main()
